6c03eccedee6f8ee76c3742799beb69b005204cd
isudzumi <isudzumi@gmail.com>
2018-01-11 10:30:56 +0900

    Update Client.java

--- ex13/Client.java
+++ ex13/Client.java
@@ -5,6 +5,8 @@ import java.net.*;
 class Client {
   public static void main(String[] args) {
     try{
+      BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
+      String data = new String(in.readLine());
       Socket mysocket = new Socket("localhost", 1234);
       BufferedReader in = new BufferedReader(new InputStreamReader(mysocket.getInputStream()));
       System.out.println("Recieved String : ");
